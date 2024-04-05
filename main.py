e0d682c04394fa757bd14bcb3d305611bba0c25e
dan005 <danil.shevelev.2004@mail.ru>
2024-04-05 12:01:32 +0500

    Ввел blueprint'ы

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from flask import Flask, Blueprint
+from flask_jwt_extended import JWTManager
+from config import JWT_SECRET_KEY
 
 from config import (
     HOST,
     PORT,
     DEBUG,
 )
-from boss import boss
-from hr import hr
+from endpoints.boss import boss
+from endpoints.hr import hr
+from endpoints.common import common
 
 app: Flask = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+jwt = JWTManager(app)
+
 
 BLUEPRINTS: list[Blueprint] = [
     boss,
     hr,
+    common,
 ]
 for blueprint in BLUEPRINTS:
     app.register_blueprint(blueprint)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
         host=HOST,
         port=PORT,
