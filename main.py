ce22f5673f11c576ff435acbde703d88dc669998
dan005 <danil.shevelev.2004@mail.ru>
2024-04-05 15:45:27 +0500

    `JWT_ACCESS_TOKEN_EXPIRES`

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, Blueprint
 from flask_jwt_extended import JWTManager
-from config import JWT_SECRET_KEY
 
 from config import (
     HOST,
     PORT,
     DEBUG,
+    JWT_SECRET_KEY,
+    JWT_ACCESS_TOKEN_EXPIRES,
 )
 from models import Base, engine
 from endpoints.boss import boss
@@ -15,6 +16,7 @@ from endpoints.login import login
 
 app: Flask = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
 jwt = JWTManager(app)
 
 
