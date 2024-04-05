83f6eeb11bdd8b970cd29e7d2932104d05686c5e
Alekron <forsakenfucker@mail.ru>
2024-04-05 12:56:29 +0500

    recruits upload endpoint

--- endpoints/hr.py
+++ endpoints/hr.py
@@ -1,18 +1,23 @@
-from flask import Blueprint
+import os
 
-__all__ = (
-    'hr',
-)
+from flask import Blueprint, request
+from config import UPLOAD_RECRUITS_FOLDER
 
-hr: Blueprint = Blueprint('hr', __name__)
+__all__ = ("hr",)
+
+hr: Blueprint = Blueprint("hr", __name__)
 
 
-@hr.route('/hr/upload_recruits', methods=['POST'])
+@hr.route("/hr/upload_recruits", methods=["POST"])
 def upload_recruits():
+    if "file" not in request.files:
+        return {"msg": "No file"}, 400
 
-    pass
+    file = request.files["file"]
+    if file.filename.endswith(".pdf"):
+        file.save(os.path.join(UPLOAD_RECRUITS_FOLDER, file.filename))
 
 
-@hr.route('/hr/update_recruit_step', methods=['PUT'])
+@hr.route("/hr/update_recruit_step", methods=["PUT"])
 def update_recruit_step():
     pass
