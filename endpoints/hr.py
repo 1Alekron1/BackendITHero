4b3165d97460b79720f4fd541155b477e0a97aa7
Alekron <forsakenfucker@mail.ru>
2024-04-05 13:20:36 +0500

    recruits upload with creation

--- endpoints/hr.py
+++ endpoints/hr.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Blueprint, request
 from config import UPLOAD_RECRUITS_FOLDER
+from models import session, Recruit
 
 __all__ = ("hr",)
 
@@ -12,12 +13,38 @@ hr: Blueprint = Blueprint("hr", __name__)
 def upload_recruits():
     if "file" not in request.files:
         return {"msg": "No file"}, 400
+    task_id = request.json().get("taskId", None)
+    for filename in request.files:
+        file = request.files[filename]
+        if file.filename.endswith(".pdf"):
+            file.save(os.path.join(UPLOAD_RECRUITS_FOLDER, file.filename))
+            recruit: Recruit = Recruit(
+                task_id=task_id, file_path=UPLOAD_RECRUITS_FOLDER + file.filename
+            )
+            session.add(recruit)
+    session.commit()
+    recruits = []
+    for recruit in session.query(Recruit).filter_by(task_id=task_id):
+        recruits.append(
+            {
+                "id": recruit.id,
+                "taskId": recruit.task_id,
+                "stepNum": recruit.step_num,
+                "fileUrl": "...",
+                "gotOffer": recruit.got_offer,
+            }
+        )
 
-    file = request.files["file"]
-    if file.filename.endswith(".pdf"):
-        file.save(os.path.join(UPLOAD_RECRUITS_FOLDER, file.filename))
+    return recruits, 200
 
 
 @hr.route("/hr/update_recruit_step", methods=["PUT"])
 def update_recruit_step():
-    pass
+    step_num = request.json.get("stepNum", None)
+    recruit_id = request.json.get("recruitId", None)
+    recruit = session.query(Recruit).filter_by(id=recruit_id).first()
+    if not recruit:
+        return {"msg": "No such recruit"}, 400
+    recruit.step_num = step_num
+    session.add(recruit)
+    session.commit()
