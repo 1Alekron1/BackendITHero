778a5365bc0c54b7abe344c379340d259fea2515
Alekron <forsakenfucker@mail.ru>
2024-04-05 12:16:14 +0500

    login fix

--- endpoints/login.py
+++ endpoints/login.py
@@ -1,24 +1,21 @@
 from flask import Blueprint, request
 
-__all__ = (
-    'login',
-)
+__all__ = ("login",)
 
 from flask_jwt_extended import create_access_token
 
-from models import User
+from models import User, session
 
-login: Blueprint = Blueprint('login', __name__)
+login: Blueprint = Blueprint("login", __name__)
+
 
 @login.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    user = User.query.filter_by(name=username).first()
-    if not user or password != user.password:
+    user = session.query(User).filter_by(username=username, password=password).first()
+    if not user:
         return {"msg": "Incorrect username or password"}, 403
 
     access_token = create_access_token(identity=username)
-    return {
-        'access_token': access_token
-    }
+    return {"access_token": access_token}
